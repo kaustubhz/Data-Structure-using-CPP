28c9d4ebb8008320c3bb6f36b80040f8f77cadad
Kaustubh Zagade <38424988+kaustubhz@users.noreply.github.com>
2019-09-12 15:40:03 +0530

    Menu Driven approach added

--- LL/LLMain.cpp
+++ LL/LLMain.cpp
@@ -1,25 +1,84 @@
-#include"LinkedList.h"
-//#include"Node.h"
+#include"LinkedList.cpp"
+#include"Node.cpp"
 
 int main()
 {
+	int choice;
 	LinkedList<int> ll;
-//	ll.AddAtBegin(11);
-//	ll.AddAtBegin(22);
-//	ll.AddAtBegin(33);
-//	ll.AddAtBegin(44);
-//	ll.DisplayAll();
-//	ll.DeleteAtBegin();
-//	ll.DisplayAll();
-	ll.AddAtEnd(11);
-	ll.AddAtEnd(22);
-	ll.AddAtEnd(33);
-	ll.AddAtEnd(44);
-	ll.DisplayAll();
-	ll.DeleteAtEnd();
-	ll.DisplayAll();
-	ll.InsertAt(3,88);
-	ll.DisplayAll();
+	do
+	{
+		cout<<"\n******MENU******"<<endl;
+		cout<<"1.AddAtBegin"<<endl;
+		cout<<"2.DeleteAtBegin"<<endl;
+		cout<<"3.AddAtEnd"<<endl;
+		cout<<"4.DeleteAtEnd"<<endl;
+		cout<<"5.InsertAt"<<endl;
+		cout<<"6.Reverse"<<endl;
+		cout<<"7.Size of linked list"<<endl;
+		cout<<"8.Display"<<endl;
+		cout<<"9.EXIT"<<endl;
+		cout<<"\nEnter your choice: ";
+		cin>>choice;
+		cin.get();
+		switch(choice)
+		{
+			case 1:{
+				       int number;
+				       cout<<"\nInsert number: ";
+				       cin>>number;
+				       ll.AddAtBegin(number);
+				       cout<<endl<<number<<" inserted at beginning";
+			       }
+			       break;
 
+			case 2:{
+				       cout<<endl<<ll.DeleteAtBegin()<<" deleted from beginning"<<endl;
+			       }
+			       break;
+			case 3:{
+				       int number;
+				       cout<<"\nInsert number: ";
+				       cin>>number;
+				       ll.AddAtEnd(number);
+				       cout<<endl<<number<<" inserted at end";
+			       }
+			       break;
+			case 4:{
+				       cout<<endl<<ll.DeleteAtEnd()<<" deleted from end"<<endl;
+			       }
+			       break;
+			case 5:{
+					int pos,number;
+					cout<<"\nEnter position: ";
+					cin>>pos;
+					cout<<"\nEnter number: ";
+					cin>>number;
+					ll.InsertAt(pos,number);
+					cout<<endl<<number<<" inserted at position "<<pos;
+			       }
+			       break;
+			case 6:{
+				ll.Reverse();
+				cout<<"\nLinked list reversed";
+			       }
+			       break;
+			case 7:{
+					cout<<"\nSize of Linked list: "<<ll.Size();
+			       }
+			       break;
+			case 8:{
+				cout<<"\nElements in Linked List"<<endl;
+				ll.DisplayAll();
+			       }
+			       break;
+			case 9:{
+			       	cout<<"\nEXIT";
+				exit(0);
+			       }
+			default: cout<<"\nPlease enter valid choice";			       			    
+			       break;
+		}
+
+	}while(choice!=9);
 	return 0;
 }
