21c99a784a7d90742830ffe5cb0efced542fc7d4
Kaustubh Zagade <kaustubhz.kz153@gmail.com>
2019-09-20 22:38:00 +0530

    Changed menu driven program. Now streams can be accepted

--- StudentwithLL/DLMain.cpp
+++ StudentwithLL/DLMain.cpp
@@ -1,76 +1,107 @@
-#include"LinkedList.h"
+#include "LinkedList.h"
 int main()
 {
 	int choice;
 	Student s;
 	int number;
-	cout<<"\nHow many streams: ";
-	cin>>number;
+	cout << "\nHow many streams: ";
+	cin >> number;
 	LinkedList<Student> ll[number];
 
-/*	for(int i=0;i<number;i++)
+	for (int i = 0; i < number; i++)
+	{		
+		ll[i].AcceptCourse();
+	}
+	for (int i = 0; i < number; i++)
 	{
-		cout<<"\nEnter a stream";
-		cin>>ll[i];
-	}*/
-
-	do
-	{
-		cout<<"\n******MENU******"<<endl;
-		cout<<"1.AddAtBegin"<<endl;
-		cout<<"2.DeleteAtBegin"<<endl;
-		cout<<"3.AddAtEnd"<<endl;
-		cout<<"4.DeleteAtEnd"<<endl;
-		cout<<"5.Size of linked list"<<endl;
-		cout<<"6.Display"<<endl;
-		cout<<"7.EXIT"<<endl;
-		cout<<"\nEnter your choice: ";
-		cin>>choice;
-		cin.get();
-		switch(choice)
+		cout << endl
+			 << "Accepting student\'s data for ";
+		ll[i].DisplayCourse();
+		cout << " stream" << endl;
+		do
 		{
-			case 1:{
-				       cout<<"\nEntering details for students"<<endl;
-				       cin>>s;
-				       cout<<s;
-				       ll[0].AddAtBegin(s);
-				       cout<<endl<<"Student record inserted at beginning";
-			       }
-			       break;
+			cout << "\n******MENU******" << endl;
+			cout << "1.AddAtBegin" << endl;
+			cout << "2.DeleteAtBegin" << endl;
+			cout << "3.AddAtEnd" << endl;
+			cout << "4.DeleteAtEnd" << endl;
+			cout << "5.Size of linked list" << endl;
+			cout << "6.Display" << endl;
+			cout << "7.EXIT" << endl;
+			cout << "\nEnter your choice: ";
+			cin >> choice;
+			cin.get();
+			switch (choice)
+			{
+			case 1:
+			{
+				cout << "\nEntering details for students" << endl;
+				cin >> s;
+				cout << s;
+				ll[i].AddAtBegin(s);
+				cout << endl
+					 << "Student record inserted at beginning";
+			}
+			break;
 
-			case 2:{	ll[0].DeleteAtBegin();
-				       cout<<endl<<" deleted from beginning"<<endl;
-			       }
-			       break;
-			case 3:{
-				       cout<<"\nEntering details for students"<<endl;
-				       cin>>s;
-				       cout<<s;
-				       ll[0].AddAtBegin(s);
-				       cout<<endl<<"Student record inserted at end";
-			       }
-			       break;
-			case 4:{
-				       ll[0].DeleteAtEnd();
-				       cout<<endl<<" deleted from end"<<endl;
-			       }
-			       break;
-			case 5:{
-					cout<<"\nSize of Linked list: "<<ll[0].Size();
-			       }
-			       break;
-			case 6:{
-				cout<<"\nElements in Linked List"<<endl;
-				ll[0].DisplayAll();
-			       }
-			       break;
-			case 7:{
-			       	cout<<"\nEXIT";
-			       }
-			default: cout<<"\nPlease enter valid choice";			       			    
-			       break;
-		}
+			case 2:
+			{
+				ll[i].DeleteAtBegin();
+				cout << endl
+					 << " deleted from beginning" << endl;
+			}
+			break;
+			case 3:
+			{
+				cout << "\nEntering details for students" << endl;
+				cin >> s;
+				cout << s;
+				ll[i].AddAtBegin(s);
+				cout << endl
+					 << "Student record inserted at end";
+			}
+			break;
+			case 4:
+			{
+				ll[i].DeleteAtEnd();
+				cout << endl
+					 << " deleted from end" << endl;
+			}
+			break;
+			case 5:
+			{
+				cout << "\nSize of Linked list: " << ll[i].Size();
+			}
+			break;
+			case 6:
+			{
+				cout << "\nElements in Linked List" << endl;
+				ll[i].DisplayAll();
+			}
+			break;
+			case 7:
+			{
+				cout << "\nEXIT";
+			}
+			break;
+			default:
+				cout << "\nPlease enter valid choice";
+				break;
+			}
 
-	}while(choice!=7);
+		} while (choice != 7);
+	}
+	cout << endl
+		 << "Displaying student\'s details along with streams" << endl;
+	for (int i = 0; i < number; i++)
+	{
+		cout << endl
+			 << "Stream: ";
+		ll[i].DisplayCourse();
+		cout << endl;
+		cout << endl
+			 << "Student\'s details" << endl;
+		ll[i].DisplayAll();
+	}
 	return 0;
 }
